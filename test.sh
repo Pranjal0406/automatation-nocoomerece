9bb8d9a12a0973f5c04ed2ba443a9f88a57f2488
Pranjal0406 <namapranjal4@gmail.com>
2024-06-12 00:28:31 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,8 +3,5 @@
 # Ensure the script exits on any error
 set -e
 
-# Navigate to the directory containing your tests (adjust as necessary)
-pip install pytest
-
 # Run pytest with specified options
 pytest -s -v testCases/Test_Login.py
